c25aca9215bad95ddb58a470bf2c8d2c89d2044f
dorian68 <labrydorian.mpsi@hotmail.fr>
2026-01-17 21:21:57 +0100

    Initial commit - n8n to Python LangGraph migration

---
+++ run.py
@@ -0,0 +1,14 @@
+FINNHUB_URL = "https://finnhub.io/api/v1/news"
+DEFAULT_TOKEN = "d325jn9r01qn0gi2h72gd325jn9r01qn0gi2h730"
+FINNHUB_TOKEN = "d325jn9r01qn0gi2h72gd325jn9r01qn0gi2h730"
+
+
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+
+
+
+
+
